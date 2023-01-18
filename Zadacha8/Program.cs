692f73861327aca70f95111abe586612e78a22bf
IlyaRedWhite <il.durmanov@yandex.ru>
2023-01-18 20:33:31 +0300

    Исправлена 8 задача

--- Zadacha8/Program.cs
+++ Zadacha8/Program.cs
@@ -1,26 +1,8 @@
-﻿//Найти все четные числа от 1 до N
-
-int oddNumbersCount = 0; //нечетные числа
-int evenNumbersCount = 0; //четные числа
-
-Console.WriteLine("Введите число ");
-int currentValue = 1;
-int N = int.Parse(Console.ReadLine());
-
-while (currentValue <= N )
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int res = 1;
+while (res < num)
 {
-    if (currentValue % 2 == 0)
-    {
-        evenNumbersCount++;
-    }
-    else
-    {
-        oddNumbersCount++;
-    }
-currentValue++;
-}
-
-
-
-
-Console.WriteLine("Количество четных чисел: " + evenNumbersCount);
+    Console.WriteLine(res + 1);
+    res += 2;
+}    
